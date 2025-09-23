b80644dc2dc4a8fbb6f7c2ff77dc888aebb30f09
zizokosaa <zeyadsalih333@gmail.com>
2025-09-23 22:02:16 +0300

    creating RinexFactory & RinexEnums

---
+++ src/rtkcorrection_dir/controllers/RinexToText/RinexEnums.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class RinexEnum(Enum):
+    RINEX_V2_10 = "RINEX_V2_10"
+    RINEX_V2_11 = "RINEX_V2_11"
+    RINEX_V2_12 = "RINEX_V2_12"
+    RINEX_V3_00 = "RINEX_V3_00"
+    RINEX_V3_02 = "RINEX_V3_02"
+    RINEX_V3_03 = "RINEX_V3_03"
+    RINEX_V3_04 = "RINEX_V3_04"
+    
---
+++ src/rtkcorrection_dir/controllers/RinexToText/RinexProviderFactory.py
@@ -0,0 +1,5 @@
+from RinexEnums import RinexEnum
+
+class RinexFactory:
+    def __inti__(self):
+        pass
